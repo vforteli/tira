f8f2b9d972f8402916c91678acf9d549b4dca86b
vforteli <vforteli@192.168.8.200>
2013-09-23 18:33:38 +0300

    added scaffold for minheap

---
+++ src/astar/MinHeap.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package astar;
+
+/**
+ *
+ * @author verne_000
+ */
+public class MinHeap
+{
+    public void Insert(int value, Coordinates coordinates)
+    {
+        
+    
+    }
+        
+    public Coordinates DeleteMin()
+    {
+        return new Coordinates();
+    }
+}
